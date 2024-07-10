c52bff1a248d0c7201547802fec448d1e04f00ad
박천일 <bagcheon-il@1001pc.local>
2024-07-10 18:01:26 +0900

    상세 화면 구성

--- lib/screen/edit_screen.dart
+++ lib/screen/edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:archive_idea_udemy/data/idea_info.dart';
+import 'package:archive_idea_udemy/database/database_helper.dart';
 import 'package:flutter/material.dart';
 
 class EditScreen extends StatefulWidget {
@@ -11,6 +12,31 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+  // 아이디어 제목
+  final TextEditingController _titleController = TextEditingController();
+
+  // 아이디어 계기
+  final TextEditingController _motiveController = TextEditingController();
+
+  // 아이디어 내용
+  final TextEditingController _contentController = TextEditingController();
+
+  // 아이디어 피드백
+  final TextEditingController _feedbackController = TextEditingController();
+
+  // 아이디어 중요도 점수 container 클릭 상태 관리 변수
+  bool isClickPoint1 = false;
+  bool isClickPoint2 = false;
+  bool isClickPoint3 = true;
+  bool isClickPoint4 = false;
+  bool isClickPoint5 = false;
+
+  // 아이디어 선택된 현재 중요도 점수 (default value = 3)
+  int priorityPoint = 3;
+
+  // database helper
+  final dbHelper = DatabaseHelper();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +59,363 @@ class _EditScreenState extends State<EditScreen> {
           style: TextStyle(color: Colors.black, fontSize: 16),
         ),
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('제목'),
+              Container(
+                margin: EdgeInsets.only(top: 8),
+                height: 41,
+                child: TextField(
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: '아이디어 제목',
+                    hintStyle: TextStyle(
+                      color: Color(0xffb4b4b4),
+                    ),
+                  ),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                  controller: _titleController,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text('아이디어를 떠올린 계기'),
+              Container(
+                margin: EdgeInsets.only(top: 8),
+                height: 41,
+                child: TextField(
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: '아이디어를 떠올린 계기',
+                    hintStyle: TextStyle(
+                      color: Color(0xffb4b4b4),
+                    ),
+                  ),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                  controller: _motiveController,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text('아이디어 내용'),
+              Container(
+                margin: EdgeInsets.only(top: 8),
+                child: TextField(
+                  maxLength: 500,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: '떠로으신 아이디어를 자유롭게 작성해주세요',
+                    hintStyle: TextStyle(
+                      color: Color(0xffb4b4b4),
+                    ),
+                  ),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                  controller: _contentController,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text('아이디어 중요도 점수'),
+              Container(
+                margin: EdgeInsets.only(top: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 50,
+                        height: 40,
+                        decoration: ShapeDecoration(
+                            color: isClickPoint1
+                                ? Color(0xffd6d6d6)
+                                : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.black),
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onTap: () {
+                        // 1. 모든 버튼 값 초기화
+                        ininClickStatus();
+                        // 2. 선택된 버튼에 대한 변수 값 및 위젯 update
+                        setState(() {
+                          priorityPoint = 1;
+                          isClickPoint1 = true;
+                        });
+                      },
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 50,
+                        height: 40,
+                        decoration: ShapeDecoration(
+                            color: isClickPoint2
+                                ? Color(0xffd6d6d6)
+                                : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.black),
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: Text(
+                          '2',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onTap: () {
+                        // 1. 모든 버튼 값 초기화
+                        ininClickStatus();
+                        // 2. 선택된 버튼에 대한 변수 값 및 위젯 update
+                        setState(() {
+                          priorityPoint = 2;
+                          isClickPoint2 = true;
+                        });
+                      },
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 50,
+                        height: 40,
+                        decoration: ShapeDecoration(
+                            color: isClickPoint3
+                                ? Color(0xffd6d6d6)
+                                : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.black),
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: Text(
+                          '3',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onTap: () {
+                        // 1. 모든 버튼 값 초기화
+                        ininClickStatus();
+                        // 2. 선택된 버튼에 대한 변수 값 및 위젯 update
+                        setState(() {
+                          priorityPoint = 3;
+                          isClickPoint3 = true;
+                        });
+                      },
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 50,
+                        height: 40,
+                        decoration: ShapeDecoration(
+                            color: isClickPoint4
+                                ? Color(0xffd6d6d6)
+                                : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.black),
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: Text(
+                          '4',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onTap: () {
+                        // 1. 모든 버튼 값 초기화
+                        ininClickStatus();
+                        // 2. 선택된 버튼에 대한 변수 값 및 위젯 update
+                        setState(() {
+                          priorityPoint = 4;
+                          isClickPoint4 = true;
+                        });
+                      },
+                    ),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 50,
+                        height: 40,
+                        decoration: ShapeDecoration(
+                            color: isClickPoint5
+                                ? Color(0xffd6d6d6)
+                                : Colors.white,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.black),
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: Text(
+                          '5',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      onTap: () {
+                        // 1. 모든 버튼 값 초기화
+                        ininClickStatus();
+                        // 2. 선택된 버튼에 대한 변수 값 및 위젯 update
+                        setState(() {
+                          priorityPoint = 5;
+                          isClickPoint5 = true;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text('유저 피드백 사항 (선택)'),
+              Container(
+                margin: EdgeInsets.only(top: 8),
+                child: TextField(
+                  maxLength: 500,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xffd9d9d9),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: '떠오르신 아이디어 기반으로\n전달받은 피드백들을 정리해주세요',
+                    hintStyle: TextStyle(
+                      color: Color(0xffb4b4b4),
+                    ),
+                  ),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                  controller: _feedbackController,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+
+              /// 아이디어 작성 완료 버튼
+              GestureDetector(
+                child: Container(
+                  height: 65,
+                  alignment: Alignment.center,
+                  child: Text('아이디어 작성 완료'),
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onTap: () async {
+                  // 아이디어 작성 (database insert) 처리
+                  String titleValue = _titleController.text.toString();
+                  String motiveValue = _motiveController.text.toString();
+                  String contentValue = _contentController.text.toString();
+                  String feedbackValue = _feedbackController.text.toString();
+
+                  // 유효성 검사 (비어 있는 필수 입력 값에 대한 체크)
+                  if (titleValue.isEmpty ||
+                      motiveValue.isEmpty ||
+                      contentValue.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('비어있는 입력 값이 존재합니다'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                    return;
+                  }
+
+                  if (widget.ideaInfo == null) {
+                    // 아이디어 정보 클래스 익스턴스 생성 후 db 삽입
+                    var ideaInfo = IdeaInfo(
+                      title: titleValue,
+                      motive: motiveValue,
+                      content: contentValue,
+                      priority: priorityPoint,
+                      feedback: feedbackValue.isNotEmpty ? feedbackValue : '',
+                      createdAt: DateTime.now().millisecondsSinceEpoch,
+                    );
+
+                    await setInsertIdeaInfo(ideaInfo);
+                    if (mounted) {
+                      // 모든 시나리오가 완료되었으니 이전화면으로 이동
+                      Navigator.pop(context);
+                    }
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Future setInsertIdeaInfo(IdeaInfo ideaInfo) async {
+    // 삼입 하는 메서드
+    await dbHelper.initDatabase();
+    await dbHelper.insertIdeaInfo(ideaInfo);
+  }
+
+  void ininClickStatus() {
+    // 클릭 상태를 초기화 해주는 함수
+    isClickPoint1 = false;
+    isClickPoint2 = false;
+    isClickPoint3 = false;
+    isClickPoint4 = false;
+    isClickPoint5 = false;
   }
 }
